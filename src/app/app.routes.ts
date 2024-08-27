d31e50a27c0a89b9dbf12afefa92c04cdf8898bb
Steve Gorline <sgorline@Steves-MBP.lan>
2024-08-27 09:38:22 -0500

    routing, etc.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { ChangeDetectionComponent } from './change-detection/change-detection.component';
+import { PipesComponent } from './pipes/pipes.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first-component', component: ChangeDetectionComponent },
+    { path: 'second-component', component: PipesComponent },
+];
